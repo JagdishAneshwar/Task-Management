c508022b6ea489defc92d1b6b5d59991c36e1eba
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-12-14 12:27:40 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-6=&3499hwytk##@wfgi)idv(2s7%#xc49sc&c2jxa0_fnq67-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
